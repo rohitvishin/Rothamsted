4e9a4d0f24c6450f2bf706d0f8b78807fd3f3366
rohit <rohitvishwakarma65@gmail.com>
2024-01-22 18:10:20 +0530

    video downgraded

--- app/video/page.tsx
+++ app/video/page.tsx
@@ -1,43 +1,9 @@
 'use client';
-import { useEffect, useRef } from 'react';
+export default function Video(){
+    return (
+      <video width="500" height="500" controls autoPlay>
+  <source src="/video/road.mp4" type="video/mp4" />
+</video>
 
-type VideoPlayerProps = {
-  videoSrc: any; // Type for the video source URL
-};
-
-const Video: React.FC<VideoPlayerProps> = ({ videoSrc }) => {
-  const videoRef = useRef<HTMLVideoElement>(null);
-
-  useEffect(() => {
-    const videoElement = videoRef.current;
-
-    const handleUserInteraction = () => {
-      if (videoElement) {
-        videoElement.play().then(() => {
-          // Autoplay started successfully
-          videoElement.muted = false; // Unmute the video
-        }).catch((error) => {
-          // Autoplay failed, handle error or display a play button
-          console.error('Autoplay failed:', error);
-        });
-      }
-    };
-
-    // Listen for user interaction (e.g., a click)
-    document.addEventListener('click', handleUserInteraction);
-
-    return () => {
-      // Cleanup: remove the event listener when the component unmounts
-      document.removeEventListener('click', handleUserInteraction);
-    };
-  }, []);
-
-  return (
-    <video ref={videoRef} width="640" height="360" controls>
-      <source src={videoSrc} type="video/mp4" />
-      Your browser does not support the video tag.
-    </video>
-  );
-};
-
-export default Video;
+    )
+}
