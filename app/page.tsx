4ad59eb4b89267366d436942b11640cc02409bb0
rohit <rohitvishwakarma65@gmail.com>
2024-01-22 18:12:38 +0530

    video 4

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,9 @@
 'use client';
-// pages/index.js
-import VideoPlayer from "./video/page";
+export default function Video(){
+    return (
+      <video width="500" height="500" controls autoPlay>
+  <source src="/video/road.mp4" type="video/mp4" />
+</video>
 
-const Home = () => {
-  const videoSrc = '/video/road.mp4';
-
-  return (
-    <div>
-      <h1>Welcome to My Website</h1>
-      <VideoPlayer videoSrc={videoSrc} />
-    </div>
-  );
-};
-
-export default Home;
-
+    )
+}
