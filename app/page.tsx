9fbb1bba8b3d53e480eecda5963bd817ddba0aa5
rohit <rohitvishwakarma65@gmail.com>
2024-01-25 14:07:03 +0530

    screen message updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 "use client";
-import { useRouter } from 'next/navigation'
+import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
 export default function Landing() {
-  const router = useRouter()
-  const [Headphone, setHeadphone] = useState(false)
+  const router = useRouter();
+  const [Headphone, setHeadphone] = useState(false);
   const camera = async () => {
     try {
       const newStream: MediaStream = await navigator.mediaDevices.getUserMedia({
@@ -17,43 +17,67 @@ export default function Landing() {
 
       // Check if the error is due to user denying camera access
       if ((error as any).name === "NotAllowedError") {
-        alert('camera permission is mendatory')
+        alert("camera permission is mendatory");
+      }
     }
   };
-};
   useEffect(() => {
     setTimeout(() => {
       camera();
     }, 1000);
-    if(Headphone===true){
+    if (Headphone === true) {
       setTimeout(() => {
-        router.push('/home')
+        router.push("/home");
       }, 3000);
     }
-  }, [Headphone])
+  }, [Headphone]);
   if (typeof window !== "undefined") {
     if (window.innerWidth <= 768) {
       return (
-        <div style={{ backgroundImage: 'url("rothamsted/BG.png")', backgroundSize: 'cover', backgroundPosition: 'center', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-         {
-          Headphone && (
-            <div style={{ backgroundColor: 'rgba(0, 0, 0, 0.7)', height: '100vh', width: '100vw', display: 'flex', flexDirection: 'column',textAlign:'center', alignItems: 'center', justifyContent: 'center' }}>
-                <img src="/rothamsted/Headphone_Icon.png" height={100} width={100} style={{}} alt="Headphone" />
-                <p style={{ color: 'white' }}>
+        <div
+          style={{
+            backgroundImage: 'url("rothamsted/BG.png")',
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          {Headphone && (
+            <div
+              style={{
+                backgroundColor: "rgba(0, 0, 0, 0.7)",
+                height: "100vh",
+                width: "100vw",
+                display: "flex",
+                flexDirection: "column",
+                textAlign: "center",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <img
+                src="/rothamsted/Headphone_Icon.png"
+                height={100}
+                width={100}
+                style={{}}
+                alt="Headphone"
+              />
+              <p style={{ color: "white" }}>
                 THIS APP IS BETTER EXPERIENCED WITH HEADPHONES
-                </p>
-              </div>
-             
-          )
-         } 
+              </p>
+            </div>
+          )}
         </div>
-      )
+      );
+    } else {
+      return (
+        <>
+          <h2>Pls use mobile</h2>
+        </>
+      );
     }
-  } else {
-    return (
-      <>
-        <h2>Pls use mobile</h2>
-      </>
-    );
   }
 }
