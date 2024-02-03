e7281a61dfdd5bd00122191c8d728596bcc635d9
rohit <rohitvishwakarma65@gmail.com>
2024-02-03 17:31:36 +0530

    test 2

--- app/species/page.js
+++ app/species/page.js
@@ -4,7 +4,12 @@ import React, { useEffect, useState } from "react";
 import { Modal } from "react-bootstrap";
 import { speciesList } from "../constant/species";
 const Species = () => {
-  return <p>Hey</p>
-};
+  const [species, setSpecies] = useState(null)
+  const [showModal, setShowModal] = useState(false);
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const spec = urlParams.get("name");
+  return spec;
 
+}
 export default Species;
