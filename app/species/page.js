c69ef2f7a3e2b6cc10f49d829c6702eb02cc7a2f
rohit <rohitvishwakarma65@gmail.com>
2024-02-03 18:50:09 +0530

    test 5

--- app/species/page.js
+++ app/species/page.js
@@ -10,51 +10,7 @@ const Species = () => {
   const searchParams = useSearchParams();
   const oldQueries = searchParams.get('name');
   const playIntro = (name) => {
-    const speciesName = speciesList.data.find((obj) => obj.name.toLowerCase() === name.toLowerCase());
-    if (speciesName) {
-      setSpecies(speciesName);
-        console.log(speciesName);
-        if (speciesName !== null) {
-          const videoElement = document.createElement("video");
-          videoElement.src = speciesName.intro;
-          videoElement.height = window.innerHeight;
-          videoElement.width = window.innerWidth;
-          videoElement.controls = true;
-          videoElement.autoplay = true;
-
-          const handleFullscreenChange = () => {
-            if (
-              document.fullScreenElement ||
-              document.webkitIsFullScreen == true ||
-              document.mozFullScreen ||
-              document.msFullscreenElement
-            ) {
-            } else {
-              document.body.removeChild(videoElement);
-              handleVideoEnd();
-              // Do whatever you want on fullscreen close, like pause or mute
-            }
-          };
-
-          document.addEventListener("fullscreenchange", handleFullscreenChange);
-          videoElement.addEventListener(
-            "webkitfullscreenchange",
-            handleFullscreenChange
-          );
-          videoElement.addEventListener(
-            "webkitendfullscreen",
-            handleFullscreenChange
-          );
-          document.addEventListener("mozfullscreenchange", handleFullscreenChange);
-          document.addEventListener("MSFullscreenChange", handleFullscreenChange);
-
-          document.body.appendChild(videoElement);
-
-          videoElement.requestFullscreen().catch((err) => {
-            console.error("Error attempting to enable fullscreen", err);
-          });
-        }
-      };
+    return <p>{name}</p>
   }
   useEffect(() => {
     
@@ -64,7 +20,7 @@ const Species = () => {
     }
   }, [oldQueries])
     
-  
+  return <p>{oldQueries}</p>
   const handleClick = (side, future, data) => {
     console.log(side);
     if (data !== null && side !== null) {
