a20118c8cff2d68fc133791788a014328c4fcf46
rohitvishin <rohitvishwakarma65@gmail.com>
2024-03-10 10:45:08 +0530

    pledge index

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -24,13 +24,8 @@ export default function Contact(){
         
         <div style={{maxHeight: '70%', overflowY: 'auto'}}>
         {
-<<<<<<< HEAD
-          speciesList.data.map((item,index)=>{
-            return <div key={index} style={{margin:10,display:"flex",flexDirection:"row",backgroundColor:'#ebf5ca',padding:5,borderRadius:10}}>
-=======
-          speciesList.data.map((item)=>(
-            <div style={{margin:10,display:"flex",flexDirection:"row",backgroundColor:'#ebf5ca',padding:5,borderRadius:10}}>
->>>>>>> 16b7a99195fcfb9e684e36d395c79d843009fa41
+          speciesList.data.map((item,index)=>(
+            <div key={index} style={{margin:10,display:"flex",flexDirection:"row",backgroundColor:'#ebf5ca',padding:5,borderRadius:10}}>
               <img
               src={item.image}
               alt={item.name}
